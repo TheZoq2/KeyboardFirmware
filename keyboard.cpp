f42dc29c99c549552f2e4b9831fd830838e8d6f8
TheZoq2 <frans.skarman@gmail.com>
2015-09-25 20:23:38 +0200

    added code to prevent keystrokes from repeating when the key is released

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,5 +1,13 @@
 #include "keyboard.h"
 
+//Some additional keycodes that won't get sent but represent modifier keys
+const int KEY_SHIFT = -1;
+const int KEY_CTRL = -2;
+const int KEY_ALT = -3;
+const int KEY_GUI = -4;
+
+const int KEY_FN = -100;
+
 //The pins that the rows are connected to
 const uint8_t Z::Board::ROW_PINS[] = {3, 4, 5, 6, 1, 2};
 const uint8_t Z::Board::ROW_AMOUNT = 6;
@@ -9,12 +17,7 @@ const uint8_t Z::Board::COL_AMOUNT = 15;
 
 const int Z::Board::READ_DELAY = 5;
 
-const int KEY_SHIFT = -1;
-const int KEY_CTRL = -2;
-const int KEY_ALT = -3;
-const int KEY_GUI = -4;
-
-const int KEY_FN = -100;
+const int16_t Z::Board::RECENT_KEY_COOLDOWN = 10;
 
 //REMEMBER TO CHANGE THE SIZE OF THIS IN THE DECLARATION ASWELL
 const int Z::Board::KEYMAP[Z::Board::ROW_AMOUNT][Z::Board::COL_AMOUNT] = 
@@ -22,25 +25,17 @@ const int Z::Board::KEYMAP[Z::Board::ROW_AMOUNT][Z::Board::COL_AMOUNT] =
         {KEY_ESC, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_DELETE},
         {KEY_TILDE, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_MINUS, KEY_EQUAL, KEY_BACKSPACE},
         {KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_LEFT_BRACE, KEY_RIGHT_BRACE, KEY_BACKSLASH},
-        {KEY_CAPS_LOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_QUOTE, 0, KEY_ENTER},
+        {KEY_FN, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_SEMICOLON, KEY_QUOTE, 0, KEY_ENTER},
         {KEY_SHIFT, KEYPAD_ASTERIX, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_PERIOD, KEY_SLASH, KEY_SHIFT, KEY_HOME},
         {KEY_CTRL, KEY_FN, KEY_GUI, KEY_ALT, 0, 0, KEY_SPACE, 0, 0, KEY_ALT, KEY_FN, KEY_GUI, KEY_CTRL, KEY_END},
     };
 
-int Z::Board::FN_AMOUNT = 4;
-int Z::Board::FN_KEYS[] = {KEY_K, KEY_J, KEY_H, KEY_L};
-int Z::Board::FN_VALUES[] = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT};
-Z::Map<int, int> Z::Board::FN_MAP(Z::Board::FN_KEYS, Z::Board::FN_VALUES, FN_AMOUNT);
+const int Z::Board::FN_AMOUNT = 4;
+const uint8_t Z::Board::FN_KEYS[] = {KEY_K, KEY_J, KEY_H, KEY_L};
+const uint8_t Z::Board::FN_VALUES[] = {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT};
 
 void Z::Board::setup()
 {
-    //Alternative mappings for when the FN key is pressed
-    //Z::Board::FN_MAP[KEY_K] = KEY_UP;
-    //Z::Board::FN_MAP[KEY_J] = KEY_DOWN;
-    //Z::Board::FN_MAP[KEY_H] = KEY_LEFT;
-    //Z::Board::FN_MAP[KEY_L] = KEY_RIGHT;
-    //
-
     //Setup all rows as ouptus
     for(uint8_t i = 0; i < ROW_AMOUNT; i++)
     {
@@ -54,6 +49,8 @@ void Z::Board::setup()
         pinMode(COL_PINS[i], INPUT_PULLUP);
         digitalWrite(COL_PINS[i], INPUT_PULLUP);
     }
+
+    recentKeyAmount = 0;
 }
 
 void Z::Board::update()
@@ -81,7 +78,7 @@ void Z::Board::update()
                 //Converting the matrix signal to a keypress
                 int keyCode = Z::Board::KEYMAP[i][n];
                 
-                //If this is a regular keypress
+                //If this is a regular keypress And the key wasn't pressed before
                 if(keyCode > 0)
                 {
                     //Add the key
@@ -109,16 +106,12 @@ void Z::Board::update()
     {
         for(int i = 0; i < pressedAmount; i++)
         {
-            int* fnAlternative = FN_MAP.getValue(pressedKeys[i]);
-            if(fnAlternative != NULL)
-            {
-                pressedKeys[i] = *fnAlternative;
-            }
+            pressedKeys[i] = translateFN(pressedKeys[i]);
         }
     }
 
-    Serial.println(KEY_H);
     updateSentKeys();
+    updateRecentKeys();
     sendKeys();
 }
 
@@ -181,6 +174,7 @@ void Z::Board::updateSentKeys()
         //The key isn't pressed anymore, remove it from the list of sent keys
         if(!isPressed) 
         {
+            addRecentKey(sentKeys[i]);
             sentKeys[i] = 0;
         }
     }
@@ -229,4 +223,66 @@ void Z::Board::handleModifier(int keyCode)
         }
     }
 }
+int Z::Board::translateFN(int keycode)
+{
+    //Loop over the keys that shoudl change when the FN key is held down
+    for(uint8_t i = 0; i < FN_AMOUNT; i++)
+    {
+        if(FN_KEYS[i] == keycode)
+        {
+            return FN_VALUES[i];
+        }
+    }
+    return keycode;
+}
+bool Z::Board::isKeyRecent(int keycode)
+{
+    for(uint8_t i = 0; i < recentKeyAmount; i++)
+    {
+        if(recentKeys[i] == keycode)
+        {
+            return true;
+        }
+    }
 
+    return false;
+}
+void Z::Board::addRecentKey(int keycode)
+{
+    //Because we know that a key will only be added to the recent keys after being pressed and it can't 
+    //be pressed if it's on the recent keys already, we don't need to check if it's on the list already
+    
+    if(keycode != 0)
+    {
+        recentKeys[recentKeyAmount] = keycode;
+        recentKeyTimes[recentKeyAmount] = RECENT_KEY_COOLDOWN;
+        recentKeyAmount++;
+
+        Serial.println(recentKeyAmount);
+    }
+}
+void Z::Board::updateRecentKeys()
+{
+    //Iterating over all the recent keys
+    for(uint8_t i = 0; i < recentKeyAmount; ++i)
+    {
+        //Decrement the time of cooldown by 1
+        recentKeyTimes[i]--;
+
+        Serial.println(recentKeyTimes[i]);
+
+        //Check if the time is 0
+        if(recentKeyTimes[i] <= 0)
+        {
+            //Move all the other keys forward
+            for(uint8_t n = i; n < recentKeyAmount; ++n)
+            {
+                recentKeys[n] = recentKeys[n + 1];
+                recentKeyTimes[n] = recentKeyTimes[n + 1];
+                
+                recentKeyAmount--;
+            }
+        }
+    }
+
+}
--- keyboard.h
+++ keyboard.h
@@ -23,10 +23,9 @@ namespace Z
 
         static const int KEYMAP[6][15];
 
-        static int FN_AMOUNT;
-        static int FN_KEYS[];
-        static int FN_VALUES[];
-        static Map<int, int> FN_MAP;
+        static const int FN_AMOUNT;
+        static const uint8_t FN_KEYS[];
+        static const uint8_t FN_VALUES[];
         
         //The delay between setting the row HIGH and reading the value
         //in microseconds
@@ -34,20 +33,32 @@ namespace Z
 
         static const uint8_t MAX_PRESSED_KEYS = 128;
         static const uint8_t MAX_SENT_KEYS = 6;
+
+        //The minimum amount of time between two keypresses of the same key
+        //in loop iterations
+        static const int16_t RECENT_KEY_COOLDOWN;
         
         ////////////////////////////////////////
         void updateSentKeys();
         void sendKeys();
         void handleModifier(int keyCode);
+        int translateFN(int keycode);
+        
+        bool isKeyRecent(int keycode);
+        void addRecentKey(int keycode);
+        void updateRecentKeys();
         ////////////////////////////////////////
 
         //The keys that are currently being pressed
         uint8_t pressedAmount;
         uint8_t pressedKeys[MAX_PRESSED_KEYS];
         uint8_t sentKeys[MAX_SENT_KEYS];
+
+        uint8_t recentKeys[MAX_PRESSED_KEYS];
+        int16_t recentKeyTimes[MAX_PRESSED_KEYS];
+        uint8_t recentKeyAmount;
         
         uint8_t currentModifier;
-
     };
 };
 #endif
